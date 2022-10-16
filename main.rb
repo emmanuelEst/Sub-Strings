211b94145a47fad859a2aa666915949831729231
emmanuelEst <70251524+emmanuelEst@users.noreply.github.com>
2022-10-16 16:45:38 +0000

    Create main method and method to remove punctuation

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-puts "Hello, World!"
+def substring(string, _dictionary)
+  no_punct = rm_punctuation(string)
+  no_punct
+  # TODO: split_string each include? dictionary[index]
+end
+
+# returns a string without any puncuation
+def rm_punctuation(string)
+  regex = /\p{Punct}/
+  split_string = string.split('')
+  without_punct = split_string.map do |char|
+    char unless char.match?(regex)
+  end
+  without_punct.join('')
+end
